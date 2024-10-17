9a387fe85cdbf9c39836c352bae25df93da8f552
hexzhen3x7 <hexzhen3x7@outlook.de>
2024-10-17 19:37:55 +0200

    Added BPI-M2B & BPI-M2U

---
+++ import-board.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# import-board.sh
+
+# Author: hexzhen3x7
+# Version: 0.1
+# Website: https://codehunterz.world
+# Github: https://github.com/bpi-codehunterz-world/Banana-Pi-Board-Import
+# Description: This script imports your board to your currently running system!
+
+
+
+reset;
+
+
+
+
+
+
+
+
+detec() {
+    echo -e 'Console > Detecting board!"'
+}
+
+
+main() {
+ 
+    echo -e 'Console > Banana Pi Board-Config Import Script Started!"'
+
+}
---
+++ import/README.md
@@ -0,0 +1,1 @@
+# Here are the board-config files located!"
